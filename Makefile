88f70e1b36fb3f626236fb27d6e7f9cd4c5a78bd
LunaStev <youngjae681@gmail.com>
2025-02-06 15:09:18 +0900

    Update

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 BOOTLOADER_SRC = boot/bootloader.asm
 BOOTLOADER_BIN = boot/bootloader.bin
-BOOTLOADER_IMG = boot/bootloader.img
+OS_IMAGE = OceanOS.img  # 출력 파일 이름 수정
 
-all: $(BOOTLOADER_IMG)
+all: $(OS_IMAGE)
 
 $(BOOTLOADER_BIN): $(BOOTLOADER_SRC)
 	nasm -f bin $(BOOTLOADER_SRC) -o $(BOOTLOADER_BIN)
 
-$(BOOTLOADER_IMG): $(BOOTLOADER_BIN)
-	dd if=$(BOOTLOADER_BIN) of=$(BOOTLOADER_IMG) bs=512 count=1 conv=notrunc
+$(OS_IMAGE): $(BOOTLOADER_BIN)
+	dd if=/dev/zero of=$(OS_IMAGE) bs=512 count=2880   # OceanOS.img로 출력 파일 수정
+	dd if=$(BOOTLOADER_BIN) of=$(OS_IMAGE) bs=512 count=1 conv=notrunc
 
 .PHONY: clean
 clean:
-	rm -f $(BOOTLOADER_BIN) $(BOOTLOADER_IMG)
+	rm -f $(BOOTLOADER_BIN) $(OS_IMAGE)
--- boot/bootloader.asm
+++ boot/bootloader.asm
@@ -1,50 +1,69 @@
-[org 0x7c00]
+[ORG 0x7C00]
 
-; Bootloader Start - Print Simple Messages
-mov ah, 0x0e
-mov al, 'H'
-int 0x10
+    ; 화면에 "Hello, OceanOS" 출력
+    mov si, msg
+    call print_string
 
-mov al, 'e'
-int 0x10
+    ; GDT 설정
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:init_protected_mode  ; Far jump로 CS 업데이트
 
-mov al, 'l'
-int 0x10
+print_string:
+    mov ah, 0x0E
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .loop
+.done:
+    ret
 
-mov al, 'l'
-int 0x10
+gdt:
+    dq 0x0000000000000000          ; Null Descriptor
+    dq 0x00CF9A000000FFFF          ; Code Segment (Base=0, Limit=4GB)
+    dq 0x00CF92000000FFFF          ; Data Segment (Base=0, Limit=4GB)
 
-mov al, 'o'
-int 0x10
+gdt_descriptor:
+    dw $ - gdt - 1
+    dd gdt
 
-mov al, ','
-int 0x10
+[BITS 32]
+init_protected_mode:
+    ; 세그먼트 레지스터 초기화
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
 
-mov al, ' '
-int 0x10
+    ; 스택 포인터 설정
+    mov esp, 0x7C00
 
-mov al, 'O'
-int 0x10
+    ; "Protected Mode!" 메시지 출력
+    mov esi, protected_msg
+    mov edi, 0xB8000               ; VGA 텍스트 버퍼 주소
+    mov ah, 0x0F                   ; 속성 (흰색 텍스트)
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    mov [edi], ax
+    add edi, 2
+    jmp .loop
+.done:
+    hlt
 
-mov al, 'c'
-int 0x10
+msg db "Hello, OceanOS", 0
+protected_msg db "Protected Mode!", 0
 
-mov al, 'e'
-int 0x10
-
-mov al, 'a'
-int 0x10
-
-mov al, 'n'
-int 0x10
-
-mov al, 'O'
-int 0x10
-
-mov al, 'S'
-int 0x10
-
-jmp 0x1000
+CODE_SEG equ 0x08
+DATA_SEG equ 0x10
 
 times 510-($-$$) db 0
 dw 0xAA55
